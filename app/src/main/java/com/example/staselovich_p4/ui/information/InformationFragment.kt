cb53c91684ee9c0a132e7fc8701b1c7fd558dfc4
Ilya Stasialovich <i.stasialovich.vironit@vironit.ru>
2021-08-09 08:25:34 +0300

    add project

--- app/src/main/java/com/example/staselovich_p4/ui/information/InformationFragment.kt
+++ app/src/main/java/com/example/staselovich_p4/ui/information/InformationFragment.kt
@@ -123,8 +123,7 @@ class InformationFragment : Fragment(R.layout.fragment_information), CoinAdapter
             FirebaseAuth.getInstance().signOut()
             val googleSignInClient = GoogleSignIn.getClient(requireContext(), gso)
             googleSignInClient.signOut()
-            val action = InformationFragmentDirections.actionInformationFragmentToRegistrationFragment()
-            findNavController().navigate(action)
+            findNavController().navigateUp()
         }
     }
     fun View.hideKeyboard() {
